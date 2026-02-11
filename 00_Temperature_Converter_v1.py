e8d1da503558e84775516e97007e546ab02017f0
SacredSeth <141595269+SacredSeth@users.noreply.github.com>
2026-02-12 11:36:39 +1300

    rough bae component

---
+++ 00_Temperature_Converter_v1.py
@@ -0,0 +1,210 @@
+from tkinter import *
+from functools import partial
+import all_constants as c
+import conversion_rounding as cr
+
+
+class Convertor():
+    """
+    Temperature conversion tool (C to F or F to C)
+    """
+
+    def __init__(self):
+        """
+        Temperature convertor GUI
+        """
+
+        self.all_calculations_list = []
+
+        self.temp_frame = Frame(padx=10, pady=10)
+        self.temp_frame.grid()
+
+        self.temp_heading = Label(self.temp_frame,
+                                  text="Temperature Convertor",
+                                  font=("Arial", "16", "bold"))
+        self.temp_heading.grid(row=0)
+
+        instructions = ("Please enter a temperature below and then press "
+                        "one of the buttons to convert it from centigrade "
+                        "to Fahrenheit.")
+        self.temp_instructions = Label(self.temp_frame,
+                                       text=instructions,
+                                       wraplength=250, width=40,
+                                       justify="left")
+        self.temp_instructions.grid(row=1)
+
+        self.temp_entry = Entry(self.temp_frame,
+                                font=("Arial", "14"))
+        self.temp_entry.grid(row=2, padx=10, pady=10)
+
+        error = "Please enter a number"
+        self.answer_error = Label(self.temp_frame, text=error,
+                                  fg="#004C99", font=("Arial", "14", "bold"))
+        self.answer_error.grid(row=3)
+
+        # conversion, help and history / export buttons
+        self.button_frame = Frame(self.temp_frame)
+        self.button_frame.grid(row=4)
+
+        # button list (button text | bg colour | command | row | column)
+        button_details_list = [
+            ["To Celsius", "#990099", lambda:self.check_temp(c.ABS_ZERO_FAHRENHEIT), 0, 0],
+            ["To Fahrenheit", "#009900", lambda:self.check_temp(c.ABS_ZERO_CELSIUS), 0, 1],
+            ["Help / Info", "#CC6600", self.to_help, 1, 0],
+            ["History / Export", "#004C99", "", 1, 1]
+        ]
+
+        # list to hold buttons once they have been made
+        self.button_ref_list = []
+
+        for item in button_details_list:
+            self.make_button = Button(self.button_frame,
+                                      text=item[0], bg=item[1],
+                                      fg="#FFFFFF", font=("Arial", "12", "bold"),
+                                      width=12, command=item[2])
+            self.make_button.grid(row=item[3], column=item[4], padx=5, pady=5)
+
+            self.button_ref_list.append(self.make_button)
+
+        # retrieve to_help button
+        self.to_help_button = self.button_ref_list[2]
+
+        # retrieve 'history / export' button and disable it at the start
+        self.to_history_button = self.button_ref_list[3]
+        self.to_history_button.config(state=DISABLED)
+
+
+    def check_temp(self, min_temp):
+        """
+        Checks temperature is valid and either invokes calculation
+        function or shows a custom error
+        :param min_temp:
+        :return:
+        """
+
+        # print("Min Temp: ", min_temp)
+
+        # Retrieve temperature to be converted
+        to_convert = self.temp_entry.get()
+        print("to convert", to_convert)
+
+        # Reset label and entry box (if we had an error)
+        self.answer_error.config(fg="#004C99", font=("Arial", "13", "bold"))
+        self.temp_entry.config(bg="#FFFFFF")
+
+        error = f"Enter a number more than / equal to {min_temp}"
+        has_errors = "no"
+
+        # checks that amount to be converted is a number above absolute zero
+        try:
+            to_convert = float(to_convert)
+            if to_convert >= min_temp:
+                error = ""
+                self.convert(min_temp, to_convert)
+            else:
+                error = "Too Low"
+        except ValueError:
+            error = "Please enter a number"
+
+        # display the error if necessary
+        if error != "":
+            self.answer_error.config(text=error, fg="#9C0000")
+            self.temp_entry.config(bg="#F4CCCC")
+            self.temp_entry.delete(0, END)
+
+
+    def convert(self, min_temp, to_convert):
+        """
+        converts the inputted temperature into Celsius / Fahrenheit
+        :param min_temp:
+        :return:
+        """
+
+        if min_temp == c.ABS_ZERO_CELSIUS:
+            answer = cr.to_fahrenheit(to_convert)
+            answer_statement = f"{to_convert}C is {answer}F"
+        else:
+            answer = cr.to_celsius(to_convert)
+            answer_statement = f"{to_convert}F is {answer}C"
+
+        # enable history export button as soon as we have a valid calculation
+        self.to_history_button.config(state=NORMAL)
+
+        self.answer_error.config(text=answer_statement)
+        self.all_calculations_list.append(answer_statement)
+        print(self.all_calculations_list)
+
+
+    def to_help(self):
+        """
+        Opens help window and disables help button
+        to prevent multiple help boxes.
+        :return:
+        """
+        DisplayHelp(self)
+
+
+class DisplayHelp:
+
+    def __init__(self, partner):
+
+        # setup dialogue box
+        self.help_box = Toplevel()
+
+        # disable help button
+        partner.to_help_button.config(state=DISABLED)
+
+        # if users press cross at top, closes help and
+        # releases help button
+        self.help_box.protocol('WM_DELETE_WINDOW',
+                               partial(self.close_help, partner))
+
+        bg_colour = "#FFE6CC"
+        self.help_frame = Frame(self.help_box, width=300, height=200, bg=bg_colour)
+        self.help_frame.grid()
+
+        self.help_heading = Label(self.help_frame,
+                                  text="Help / Information",
+                                  font=("Arial", "14", "bold"),
+                                  bg=bg_colour)
+        self.help_heading.grid(row=0)
+
+        help_instructions = ("To use the program, simply enter the temperature "
+                             "you wish to convert and then choose to convert to "
+                             "either degrees Celsius (centigrade) or Fahrenheit."
+                             "\n\n"
+                             "Note that -273 degrees C (-459 F) is absolute zero "
+                             "(the coldest possible temperature). If you try to "
+                             "convert a temperature that is less than -273 degrees "
+                             "C, you will get an error message. \n\n"
+                             "To see your "
+                             "calculation history and export it to a text file, please "
+                             "click the 'History / Export' button.")
+        self.help_text = Label(self.help_frame,
+                               text=help_instructions,
+                               wraplength=350,
+                               justify="left",
+                               bg=bg_colour)
+        self.help_text.grid(row=1, padx=10)
+
+        self.close_button = Button(self.help_frame, text="Close",
+                                   bg="#CC6600", fg="#FFFFFF",
+                                   font=("Arial", "12", "bold"),
+                                   command=partial(self.close_help, partner))
+        self.close_button.grid(row=2, padx=10, pady=10)
+
+
+
+    def close_help(self, partner):
+        """closes help box and re-enables help button"""
+        # Put help button back to normal
+        partner.to_help_button.config(state=NORMAL)
+        self.help_box.destroy()
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    Convertor()
+    root.mainloop()
